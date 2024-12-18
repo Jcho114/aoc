48c581b1a7140f0fec0ef4fb915ba5c002e57982
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2024-12-18 00:00:23 -0500

    day 2 part 1

---
+++ day2/p1/go.mod
@@ -0,0 +1,3 @@
+module aoc/day2/p1
+
+go 1.22.2
---
+++ day2/p1/main.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+import "os"
+import "log"
+import "bufio"
+import "strings"
+import "strconv"
+
+func isReportSafe(report []int) bool {
+	checkDecreasing := report[1] < report[0]
+	for i := 1; i < len(report); i++ {
+		if checkDecreasing && report[i] >= report[i-1] {
+			return false
+		}
+		if !checkDecreasing && report[i] <= report[i-1] {
+			return false
+		}
+		var difference int
+		if report[i] > report[i-1] {
+			difference = report[i] - report[i-1]
+		} else {
+			difference = report[i-1] - report[i]
+		}
+		if difference < 1 || difference > 3 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	file, err := os.Open("input")
+	if err != nil {
+		log.Fatalf("error reading file")
+	}
+	defer file.Close()
+
+	total := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		stringNumbers := strings.Split(line, " ")
+		var numbers []int
+		for i := 0; i < len(stringNumbers); i++ {
+			number, err := strconv.Atoi(stringNumbers[i])
+			if err != nil {
+				log.Fatalf("error in parsing line")
+			}
+			numbers = append(numbers, number)
+		}
+		if isReportSafe(numbers) {
+			total += 1
+		}
+	}
+
+	fmt.Println(total)
+}
